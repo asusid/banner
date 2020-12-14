58fe95b0f0e389cc78c04e05dde220a2d4d74e5a
asusid <76002181+asusid@users.noreply.github.com>
2020-12-14 23:49:08 +0700

    Create banner-300.js

---
+++ banner-300.js
@@ -0,0 +1,9 @@
+var ifrm = document.createElement("iframe");
+ifrm.setAttribute("src", "https://asusid.xyz/banner/vivo-banner-300.php");
+ifrm.style.width = "300px";
+ifrm.style.height = "300px";
+ifrm.style.overflow = "hidden";
+ifrm.scrolling = "none";
+
+var div = document.getElementById("banner-asus-index-300");
+div.appendChild(ifrm);
