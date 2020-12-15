abaa0c44b1ea74f4b2ea7643f18b20b03b13bd12
asusid <76002181+asusid@users.noreply.github.com>
2020-12-15 22:25:11 +0700

    Update 300x300.js

--- 300x300.js
+++ 300x300.js
@@ -1,7 +1,7 @@
 var ifrm = document.createElement("iframe");
 ifrm.setAttribute("src", "https://asusid.xyz/banner/vivo-banner-300.php");
 ifrm.style.width = "360px";
-ifrm.style.height = "360px";
+ifrm.style.height = "313px";
 ifrm.style.overflow = "hidden";
 ifrm.scrolling = "none";
 
