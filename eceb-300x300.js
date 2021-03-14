9a54d4f8dd6fcdee094cc8ad3dc7509eed8ffc59
asusid <76002181+asusid@users.noreply.github.com>
2021-03-14 23:01:18 +0700

    Create eceb-300x300.js

---
+++ eceb-300x300.js
@@ -0,0 +1,9 @@
+var ifrm = document.createElement("iframe");
+ifrm.setAttribute("src", "https://asusid.xyz/banner/eceb-banner-300.php");
+ifrm.style.width = "300px";
+ifrm.style.height = "300px";
+ifrm.style.overflow = "hidden";
+ifrm.scrolling = "none";
+
+var div = document.getElementById("banner-asus-index-300");
+div.appendChild(ifrm);
