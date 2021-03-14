1440e694774fb663a22d9db0dd0d0101850f2658
asusid <76002181+asusid@users.noreply.github.com>
2021-03-14 23:07:01 +0700

    Create eceb-728x90.js

---
+++ eceb-728x90.js
@@ -0,0 +1,9 @@
+var ifrm = document.createElement("iframe");
+ifrm.setAttribute("src", "https://asusid.xyz/banner/eceb-banner-728.php");
+ifrm.style.width = "728px";
+ifrm.style.height = "90px";
+ifrm.style.overflow = "hidden";
+ifrm.scrolling = "none";
+
+var div = document.getElementById("banner-asus-index-728");
+div.appendChild(ifrm);
